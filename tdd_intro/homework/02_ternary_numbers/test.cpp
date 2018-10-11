c71f73449699a59519e4a0bed552e1c50647ec4e
Daria Tkachenko <daria.tkachenko19@gmail.com>
2018-10-11 10:34:17 +0300

    2: green

--- tdd_intro/homework/02_ternary_numbers/test.cpp
+++ tdd_intro/homework/02_ternary_numbers/test.cpp
@@ -17,6 +17,11 @@ The last place in a ternary number is the 1's place. The second to last is the 3
 If your language provides a method in the standard library to perform the conversion, pretend it doesn't exist and implement it yourself.
 */
 
+int ConvertTernaryToDecimal(const std::string& ternaryNumber)
+{
+    return 1;
+}
+
 
 TEST(TernaryNumber, BaseTest)
 {
