eaaeefa88bd7912b4a51d01790b885dc0fa72046
Daria Tkachenko <daria.tkachenko19@gmail.com>
2018-10-11 10:32:19 +0300

    2: red

--- tdd_intro/homework/02_ternary_numbers/test.cpp
+++ tdd_intro/homework/02_ternary_numbers/test.cpp
@@ -16,3 +16,9 @@ The last place in a ternary number is the 1's place. The second to last is the 3
 
 If your language provides a method in the standard library to perform the conversion, pretend it doesn't exist and implement it yourself.
 */
+
+
+TEST(TernaryNumber, BaseTest)
+{
+    EXPECT_EQ(1, ConvertTernaryToDecimal("1"));
+}
