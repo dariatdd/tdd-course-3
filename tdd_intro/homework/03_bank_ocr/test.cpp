a0f24dedf625825f5b1fd5c0b87e61f289224dcb
Daria Tkachenko <daria.tkachenko19@gmail.com>
2018-10-18 08:55:16 +0300

    3: refactored

--- tdd_intro/homework/03_bank_ocr/test.cpp
+++ tdd_intro/homework/03_bank_ocr/test.cpp
@@ -91,110 +91,110 @@ const unsigned short g_digitLen = 3;
 const unsigned short g_linesInDigit = 3;
 struct Digit
 {
-    std::string lines[g_linesInDigit];
+    std::vector<std::string> lines;
 };
 
 const unsigned short g_digitsOnDisplay = 9;
 struct Display
 {
-    std::string lines[g_linesInDigit];
+    std::vector<std::string> lines;
 };
 
-const Digit s_digit0 = { " _ ",
-                         "| |",
-                         "|_|"
-                       };
-const Digit s_digit1 = { "   ",
-                         "  |",
-                         "  |"
-                       };
-const Digit s_digit2 = { " _ ",
-                         " _|",
-                         "|_ "
-                       };
-const Digit s_digit3 = { " _ ",
-                         " _|",
-                         " _|"
-                       };
-const Digit s_digit4 = { "   ",
-                         "|_|",
-                         "  |"
-                       };
-const Digit s_digit5 = { " _ ",
-                         "|_ ",
-                         " _|"
-                       };
-const Digit s_digit6 = { " _ ",
-                         "|_ ",
-                         "|_|"
-                       };
-const Digit s_digit7 = { " _ ",
-                         "  |",
-                         "  |"
-                       };
-const Digit s_digit8 = { " _ ",
-                         "|_|",
-                         "|_|"
-                       };
-const Digit s_digit9 = { " _ ",
-                         "|_|",
-                         " _|"
-                       };
+const Digit s_digit0 = {{ " _ ",
+                          "| |",
+                          "|_|"
+                           }};
+const Digit s_digit1 = {{ "   ",
+                          "  |",
+                          "  |"
+                       }};
+const Digit s_digit2 = {{ " _ ",
+                          " _|",
+                          "|_ "
+                       }};
+const Digit s_digit3 = {{ " _ ",
+                          " _|",
+                          " _|"
+                       }};
+const Digit s_digit4 = {{ "   ",
+                          "|_|",
+                          "  |"
+                       }};
+const Digit s_digit5 = {{ " _ ",
+                          "|_ ",
+                          " _|"
+                       }};
+const Digit s_digit6 = {{ " _ ",
+                          "|_ ",
+                          "|_|"
+                       }};
+const Digit s_digit7 = {{ " _ ",
+                          "  |",
+                          "  |"
+                       }};
+const Digit s_digit8 = {{ " _ ",
+                          "|_|",
+                          "|_|"
+                       }};
+const Digit s_digit9 = {{ " _ ",
+                          "|_|",
+                          " _|"
+                       }};
 
-const Display s_displayAll0 = { " _  _  _  _  _  _  _  _  _ ",
-                                "| || || || || || || || || |",
-                                "|_||_||_||_||_||_||_||_||_|"
-};
+const Display s_displayAll0 = {{ " _  _  _  _  _  _  _  _  _ ",
+                                 "| || || || || || || || || |",
+                                 "|_||_||_||_||_||_||_||_||_|"
+}};
 
-const Display s_displayAll1 = { "                           ",
-                                "  |  |  |  |  |  |  |  |  |",
-                                "  |  |  |  |  |  |  |  |  |"
-};
+const Display s_displayAll1 = {{ "                           ",
+                                 "  |  |  |  |  |  |  |  |  |",
+                                 "  |  |  |  |  |  |  |  |  |"
+}};
 
-const Display s_displayAll2 = {  " _  _  _  _  _  _  _  _  _ ",
+const Display s_displayAll2 = {{  " _  _  _  _  _  _  _  _  _ ",
+                                  " _| _| _| _| _| _| _| _| _|",
+                                  "|_ |_ |_ |_ |_ |_ |_ |_ |_ "
+}};
+
+const Display s_displayAll3 = {{ " _  _  _  _  _  _  _  _  _ ",
                                  " _| _| _| _| _| _| _| _| _|",
-                                 "|_ |_ |_ |_ |_ |_ |_ |_ |_ "
-};
+                                 " _| _| _| _| _| _| _| _| _|"
+}};
 
-const Display s_displayAll3 = { " _  _  _  _  _  _  _  _  _ ",
-                                " _| _| _| _| _| _| _| _| _|",
-                                " _| _| _| _| _| _| _| _| _|"
-};
+const Display s_displayAll4 = {{ "                           ",
+                                 "|_||_||_||_||_||_||_||_||_|",
+                                 "  |  |  |  |  |  |  |  |  |"
+}};
 
-const Display s_displayAll4 = { "                           ",
-                                "|_||_||_||_||_||_||_||_||_|",
-                                "  |  |  |  |  |  |  |  |  |"
-};
+const Display s_displayAll5 = {{ " _  _  _  _  _  _  _  _  _ ",
+                                 "|_ |_ |_ |_ |_ |_ |_ |_ |_ ",
+                                 " _| _| _| _| _| _| _| _| _|"
+}};
 
-const Display s_displayAll5 = { " _  _  _  _  _  _  _  _  _ ",
-                                "|_ |_ |_ |_ |_ |_ |_ |_ |_ ",
-                                " _| _| _| _| _| _| _| _| _|"
-};
+const Display s_displayAll6 = {{ " _  _  _  _  _  _  _  _  _ ",
+                                 "|_ |_ |_ |_ |_ |_ |_ |_ |_ ",
+                                 "|_||_||_||_||_||_||_||_||_|"
+}};
 
-const Display s_displayAll6 = { " _  _  _  _  _  _  _  _  _ ",
-                                "|_ |_ |_ |_ |_ |_ |_ |_ |_ ",
-                                "|_||_||_||_||_||_||_||_||_|"
-};
+const Display s_displayAll7 = {{ " _  _  _  _  _  _  _  _  _ ",
+                                 "  |  |  |  |  |  |  |  |  |",
+                                 "  |  |  |  |  |  |  |  |  |"
+}};
 
-const Display s_displayAll7 = { " _  _  _  _  _  _  _  _  _ ",
-                                "  |  |  |  |  |  |  |  |  |",
-                                "  |  |  |  |  |  |  |  |  |"
-};
+const Display s_displayAll8 = {{ " _  _  _  _  _  _  _  _  _ ",
+                                 "|_||_||_||_||_||_||_||_||_|",
+                                 "|_||_||_||_||_||_||_||_||_|"
+}};
 
-const Display s_displayAll8 = { " _  _  _  _  _  _  _  _  _ ",
-                                "|_||_||_||_||_||_||_||_||_|",
-                                "|_||_||_||_||_||_||_||_||_|"
-};
+const Display s_displayAll9 = {{ " _  _  _  _  _  _  _  _  _ ",
+                                 "|_||_||_||_||_||_||_||_||_|",
+                                 " _| _| _| _| _| _| _| _| _|"
+}};
 
-const Display s_displayAll9 = { " _  _  _  _  _  _  _  _  _ ",
-                                "|_||_||_||_||_||_||_||_||_|",
-                                " _| _| _| _| _| _| _| _| _|"
-};
-
-const Display s_display123456789 = { "    _  _     _  _  _  _  _ ",
-                                     "  | _| _||_||_ |_   ||_||_|",
-                                     "  ||_  _|  | _||_|  ||_| _|"
-};
+const Display s_display123456789 = {{ "    _  _     _  _  _  _  _ ",
+                                      "  | _| _||_||_ |_   ||_||_|",
+                                      "  ||_  _|  | _||_|  ||_| _|"
+}};
 
 bool CheckDigitLine(const std::string& line, const std::string& lineToCompare)
 {
@@ -208,7 +208,11 @@ bool CheckDigitLine(const std::string& line, const std::string& lineToCompare)
 
 unsigned short ConvertDigit(const Digit& digit)
 {
-    throw std::exception("Invalid format");
+    if(digit.lines.empty())
+    {
+        throw std::exception("Invalid format");
+    }
+    return -1;
 }
 
 TEST(CheckDigitLine, EmptyString)
@@ -236,3 +240,4 @@ TEST(ConvertDigit, EmptyDigit)
     Digit g;
     EXPECT_THROW(ConvertDigit(g), std::exception);
 }
+
