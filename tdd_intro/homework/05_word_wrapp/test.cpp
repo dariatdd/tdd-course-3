a12afae8721f7407a7c957d9884a6b506f617209
Daria Tkachenko <daria.tkachenko19@gmail.com>
2018-10-31 17:29:27 +0200

    5: refactored

--- tdd_intro/homework/05_word_wrapp/test.cpp
+++ tdd_intro/homework/05_word_wrapp/test.cpp
@@ -41,13 +41,13 @@ WrappedStrings WrapString(const std::string& str, size_t wrapLength)
             cur.pop_back();
         }
 
-        if(!cur.empty() && cur.front() == ' ')
-        {
-            cur = cur.substr(1);
-        }
-
         if(!cur.empty())
         {
+            if(cur.front() == ' ')
+            {
+                cur = cur.substr(1);
+            }
+
             result.push_back(cur);
         }
     }
